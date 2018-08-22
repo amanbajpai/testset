f1d85413685006da101a1e74571ec66bf33d1460
akshaydashore <akshaydashore@cdnsol.com>
2018-08-22 16:34:29 +0530

    design login screen and added custom views

--- app/src/main/java/com/keykeep/app/views/activity/LoginActivity.java
+++ app/src/main/java/com/keykeep/app/views/activity/LoginActivity.java
@@ -3,19 +3,37 @@ package com.keykeep.app.views.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.keykeep.app.R;
+import com.keykeep.app.views.custom_view.CustomActionBar;
 
 /**
  * Created by akshaydashore on 22/8/18
  */
-
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+        setCustomActionBar();
     }
 
+    /**
+     * set Custom tool bar
+     */
+    private void setCustomActionBar() {
+        CustomActionBar customActionBar = new CustomActionBar(this);
+        customActionBar.setActionbar("Login", false, false, this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+
+    }
 
 
 }
---
+++ app/src/main/java/com/keykeep/app/views/custom_view/StyledEditText.java
@@ -0,0 +1,75 @@
+package com.keykeep.app.views.custom_view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.EditText;
+
+import com.keykeep.app.R;
+
+/**
+ * Created by akshaydashore on 22/8/18
+ */
+
+public class StyledEditText extends EditText {
+
+    private final String TAG = "StyledEdiText";
+    private final String DEFAULT_FONT = "roboto_regular.ttf";
+
+    public StyledEditText(Context context) {
+        super(context);
+    }
+
+    public StyledEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        setCustomFont(context, attrs);
+    }
+
+    public StyledEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        setCustomFont(context, attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public StyledEditText(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+        setCustomFont(context, attrs);
+    }
+
+
+    public void setCustomFont(Context context, AttributeSet attrs) {
+        try {
+            String customFont = getCustomFont(context, attrs);
+            if (customFont != null) {
+                Typeface face = Typeface.createFromAsset(context.getAssets(), customFont);
+                this.setTypeface(face);
+            } else {
+                Typeface face = Typeface.createFromAsset(context.getAssets(), DEFAULT_FONT);
+                this.setTypeface(face);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String getCustomFont(Context context, AttributeSet attrs) {
+        TypedArray ta = null;
+
+        try {
+            ta = context.obtainStyledAttributes(attrs, R.styleable.TextElement, 0, 0);
+            String fontName = ta.getString(R.styleable.TextElement_fontName);
+            return fontName;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            ta.recycle();
+        }
+        return null;
+    }
+}
