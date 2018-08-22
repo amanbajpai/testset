6b200ec3af0afa31fdddff8461c937420fd5f0ab
akshaydashore <akshaydashore@cdnsol.com>
2018-08-22 12:13:39 +0530

    gradle rooting changes

---
+++ app/src/main/java/com/keykeep/app/views/activity/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.keykeep.app.views.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by akshaydashore on 22/8/18
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+
+}
